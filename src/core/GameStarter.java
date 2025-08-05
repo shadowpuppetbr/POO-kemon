fe77c861fff51a65667d5057aff2d66400b2245e
Gustavo <gustavoleon2001@gmail.com>
2025-08-05 00:29:06 -0300

    [feat] add basic structure

---
+++ src/core/GameStarter.java
@@ -0,0 +1,15 @@
+package core;
+
+import game.Game;
+
+public class GameStarter {
+    public static void main(String[] args) {
+    Game gm = new Game();
+
+    gm.exec();
+
+
+
+    }
+}
+
---
+++ src/game/Game.java
@@ -0,0 +1,9 @@
+package game;
+
+public class Game {
+
+    public void  exec(){
+        System.out.println("Hello world");
+
+    }
+}
